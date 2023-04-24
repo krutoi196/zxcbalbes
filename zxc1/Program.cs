b6660f21f1873c286e00f21c0fdd07056cdfa448
43m11 <43m11@43M11>
2023-04-24 16:35:51 +0300

    change code

--- zxc1/Program.cs
+++ zxc1/Program.cs
@@ -7,6 +7,11 @@ namespace zxc1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!1");
+            Console.WriteLine("Hello World!2");
+            Console.WriteLine("Hello World!3");
+            Console.WriteLine("Hello World!4");
+
         }
     }
 }
